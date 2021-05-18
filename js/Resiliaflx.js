3f33184b729dd751dd41b4306f6916745831f224
topre <fpteixeira@outlook.com.br>
2021-05-18 12:24:52 -0300

    Criação das paginas cadastro e recupera senha, ainda com algumas atualizações css a fazer

--- js/Resiliaflx.js
+++ js/Resiliaflx.js
@@ -1,22 +1,35 @@
 const button = document.querySelector('#enviar')
+const cadInicialInvisivel = document.querySelector(`.cadastro-inicial.invisivel`)
+const cadCepInvisivel = document.querySelector(`.cadastro-endereco.invisivel`)
+const confirmaSenha = document.querySelector(`#confirmaSenha`).value
+
 // evento de validação
 button.addEventListener('click', function (event) {
   event.preventDefault()
   const senha = document.querySelector('#senha').value
-  const confirmaSenha = document.querySelector(`#confirmaSenha`)
+  
   const email = document.querySelector('#email').value
   validaEmailESenha(email, senha)
+  
+  
+
+  
 })
 
 class Usuario { // classe de usuário
-  constructor(email, senha) {
-    ;(this._email = email), (this._senha = senha)
+  constructor(email, senha, confirmaSenha) {
+    this._email = email
+    this._senha = senha
+    this._confirmaSenha = confirmaSenha
   }
   get email() {
     return this._email
   }
   get senha() {
     return this._senha
+  }
+  get confirmaSenha(){
+    return this._confirmaSenha
   }
 }
 function validaEmailUsuario(email) { //validação de email
@@ -48,6 +61,10 @@ function validaSenhaUsuario(senha1) { //validação de senha
       break
     } else if (senha1.length > 6) {
       return true
+    } else if (senha1.value === confirmaSenha.value){
+      
+      console.log(ok)
+      return true
     }
   }
   return true
@@ -55,8 +72,12 @@ function validaSenhaUsuario(senha1) { //validação de senha
 
 function validaEmailESenha(email, senha) { //preenchimento de email e senha caso sejam válidos no constructor da classe
     // const usuario1;
-  if (validaEmailUsuario(email) == true && validaSenhaUsuario(senha) == true) {
+  if (validaEmailUsuario(email) == true && validaSenhaUsuario(senha) == true)  {
     console.log('Bem vindo a Resíliaflix! Seu cinema pessoal!')
     usuario1 = new Usuario(email, senha)
   }
+}
+function formsVisiveis (){
+  cadCepInvisivel.classList.remove('cadastro-endereco.invisivel')
+  cadInicialInvisivel.classList.add(`cadastro-inicial.invisivel`)
 }
