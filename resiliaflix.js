ca22b2de64222b5d96ca51aaa98a02fb84df5be8
natalia lima <nlima352@gmail.com>
2021-05-19 00:00:50 -0300

    Organizando os detalhes do projeto

--- resiliaflix.js
+++
@@ -1,7 +0,0 @@
-
-
-//    <input type="text" id="senha" placeholder="Senha">
-//<input type="email" required id="email" placeholder="E-mail">
-//<button id="submit" alt="submit"> OK</button>
-//
-//
