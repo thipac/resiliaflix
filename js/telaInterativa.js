2ed902a8cae26115c1643c6b8958eacd2b46fee9
topre <fpteixeira@outlook.com.br>
2021-05-20 13:50:57 -0300

    ultimas atualizações,deixando a página mais interativa

---
+++ js/telaInterativa.js
@@ -0,0 +1,27 @@
+$(`#enviar`).on(`click`,function(){
+
+    
+    $(`#formsEndereco`).removeClass("invisivel")
+    $(`.inicioInvisivel`).css({"display":"none"})
+})
+$(`#enviarCadastro`).on(`click`,function(){
+    $(`#formsEndereco`).addClass("invisivel")
+    $(`.cadSucesso`).removeClass(`oculto`)
+})
+
+$("form").submit(false);
+
+$(`#escolhaEmail`).on(`click`, function(){
+    $(`#mostraEmail`).removeClass("emailInvisivel")
+    $(`#mostraTel`).addClass("telefoneInvisivel")
+})
+
+$(`#escolhaTel`).on(`click`, function(){
+    $(`#mostraEmail`).addClass("emailInvisivel")
+    $(`#mostraTel`).removeClass("telefoneInvisivel")
+})
+
+$(`#btnRecupera`).on(`click`,function(){
+    $(`.caixa-principal`).css({"display":"none"})
+    $(`.cadSucesso`).removeClass(`oculto`)
+})
